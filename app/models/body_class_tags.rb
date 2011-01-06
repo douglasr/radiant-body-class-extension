8a4cc82840e3a753d92d25c46b4e631f28bf2ca6
Douglas Robertson <dr@edgeoftheearth.com>
2011-01-06 12:20:35 -0700

    Only inherit the parent body_class if the tag inherit attribute is set to true or all

--- app/models/body_class_tags.rb
+++ app/models/body_class_tags.rb
@@ -2,7 +2,7 @@ module BodyClassTags
   include Radiant::Taggable
   
   desc %{
-    Expands if a <pre><r:body_class [inherit="true|false"]/></pre> call would return a body class string.
+    Expands if a <pre><r:body_class [inherit="true|false|all"]/></pre> call would return a body class string.
     The <pre><r:unless_body_class /></pre> tag is also available to be used.
   }
   tag "if_body_class" do |tag|
@@ -13,6 +13,7 @@ module BodyClassTags
       tag.expand unless body_class.blank?
     end
   end
+
   tag "unless_body_class" do |tag|
     body_class = get_body_class(tag)
     if tag.attr['equal']
@@ -22,18 +23,24 @@ module BodyClassTags
     end
   end
   
-  desc "Render the body class text for the current page."
+  desc %{
+    Render the body class text for the current page. If the current page doesn't have anything in its
+    body_class attribute, it will retrieve the value from any parents if the inherit attribute is set
+    to 'true' or 'all' as such: <pre><r:body_class [inherit="true|false|all"]/></pre>
+  }
   tag "body_class" do |tag|
     get_body_class(tag)
   end
   
   def get_body_class(tag)
     body_class = tag.locals.page.body_class.name || ''
-    
-    parent = tag.locals.page.parent
-    while parent
-      body_class += ' '+parent.body_class.name unless parent.body_class.name.blank?
-      parent = parent.parent
+
+    if (tag.attr['inherit'] == 'true' || tag.attr['inherit'] == 'all')
+      parent = tag.locals.page.parent
+      while parent
+        body_class += " #{parent.body_class.name}" unless parent.body_class.name.blank?
+        parent = parent.parent if tag.attr['inherit'] == 'all'
+      end
     end
     
     body_class

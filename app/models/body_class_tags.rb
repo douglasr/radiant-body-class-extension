13cb5764f619d63e1054c418181822351ed5b182
Aimerick DESDOIT <aimerick@lunaweb.fr>
2011-01-07 09:29:51 +0100

    synchronisation avec le dépôt original de douglasr et ajout de la valeur all par défaut sur le paramètre inherit des tags

--- app/models/body_class_tags.rb
+++ app/models/body_class_tags.rb
@@ -1,8 +1,9 @@
 module BodyClassTags
+  
   include Radiant::Taggable
   
   desc %{
-    Expands if a <pre><r:body_class [inherit="true|false"]/></pre> call would return a body class string.
+    Expands if a <pre><r:body_class [inherit="true|false|all"]/></pre> call would return a body class string.
     The <pre><r:unless_body_class /></pre> tag is also available to be used.
   }
   tag "if_body_class" do |tag|
@@ -13,6 +14,7 @@ module BodyClassTags
       tag.expand unless body_class.blank?
     end
   end
+
   tag "unless_body_class" do |tag|
     body_class = get_body_class(tag)
     if tag.attr['equal']
@@ -22,21 +24,36 @@ module BodyClassTags
     end
   end
   
-  desc "Render the body class text for the current page."
+  desc %{
+    Render the body class text for the current page. If the current page doesn't have anything in its
+    body_class attribute, it will retrieve the value from any parents if the inherit attribute is set
+    to 'true' or 'all' as such: <pre><r:body_class [inherit="true|false|all"]/></pre>
+    
+    true : get first no-blank body_class from parents 
+    all (default) : get all no-blank from parents
+  }
   tag "body_class" do |tag|
     get_body_class(tag)
   end
   
   def get_body_class(tag)
     body_class = tag.locals.page.body_class.name || ''
+
+    inherit = tag.attr['inherit'] ? tag.attr['inherit'] : 'all'
     
-    parent = tag.locals.page.parent
-    while parent
-      body_class += ' '+parent.body_class.name unless parent.body_class.name.blank?
-      parent = parent.parent
+    if (inherit == 'true' && body_class.blank?) || inherit == 'all'
+      parent = tag.locals.page.parent
+      while parent
+        body_class += " #{parent.body_class.name}" unless parent.body_class.name.blank?
+        if inherit == 'all' || body_class.blank?
+          parent = parent.parent
+        else
+          parent = nil
+        end
+      end
     end
     
-    body_class
+    body_class.strip
   end
 
 end
